59851c3cef15a133971281a43431ac914412d42b
ilyalyai <lyapilya@yandex.ru>
2023-07-24 01:27:17 +0300

    Initial сommit

---
+++ PettyProject/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿
+namespace PettyProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button18 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(386, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(426, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(262, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(345, 174);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(426, 174);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(426, 203);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(344, 203);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(262, 203);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(566, 145);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "+";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(566, 174);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 13;
+            this.button11.Text = "-";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(566, 203);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 14;
+            this.button12.Text = "*";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(566, 233);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 15;
+            this.button13.Text = "\\";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(566, 291);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 16;
+            this.button14.Text = "=";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.Button3Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(426, 291);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 17;
+            this.button15.Text = ".";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.Button15Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(566, 99);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(75, 23);
+            this.button16.TabIndex = 18;
+            this.button16.Text = "remove";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(566, 70);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(75, 23);
+            this.button17.TabIndex = 19;
+            this.button17.Text = "reset";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 361);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 21;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(41, 284);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(140, 23);
+            this.button18.TabIndex = 22;
+            this.button18.Text = "Узнать погоду";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Moscow",
+            "London"});
+            this.comboBox1.Location = new System.Drawing.Point(51, 235);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Выберите город:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 546);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PettyProject/Form1.cs
@@ -0,0 +1,222 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using WeatherConsoleApplication;
+
+namespace PettyProject
+{
+    public partial class Form1 : Form
+    {
+        private string firstInt = "";
+        private string secondInt = "";
+        private string whatToDo = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button Btext = (Button)sender;
+            if (!firstInt.EndsWith(" "))
+                firstInt += Btext.Text;
+            else if (!(label1.Text.Contains("+") || label1.Text.Contains("-") || label1.Text.Contains("*") || label1.Text.Contains(@"\")))
+            {
+                label1.Text = Btext.Text;
+                firstInt = Btext.Text;
+                return;
+            }
+            else
+                secondInt += Btext.Text;
+
+            label1.Text += Btext.Text;
+        }
+
+        private void Button2Click(object sender, EventArgs e)
+        {
+            Button Btext = (Button)sender;
+            label1.Text += " ";
+            label1.Text += Btext.Text + " ";
+            if (!firstInt.EndsWith(" "))
+            {
+                firstInt += " ";
+                whatToDo = Btext.Text;
+                return;
+            }
+            else if (secondInt == "")
+            {
+                whatToDo = Btext.Text;
+                return;
+            }
+            else
+            {
+                float result = 0;
+                switch (whatToDo)
+                {
+                    case "+":
+                        {
+                            result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) +
+                                     float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                            break;
+                        }
+
+                    case "-":
+                        {
+                            result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) -
+                                     float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                            break;
+                        }
+
+                    case "*":
+                        {
+                            result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) *
+                                     float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                            break;
+                        }
+
+                    case @"\":
+                        {
+                            result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) /
+                                     float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                            break;
+                        }
+                }
+
+                label1.Text = result.ToString(CultureInfo.InvariantCulture) + Btext.Text;
+                secondInt = "";
+                firstInt = result.ToString(CultureInfo.InvariantCulture) + " ";
+                whatToDo = Btext.Text;
+            }
+        }
+
+        private void Button3Click(object sender, EventArgs e)
+        {
+            if (firstInt.Length == 0 || secondInt.Length == 0 || whatToDo.Length == 0)
+                return;
+            float result = 0;
+            switch (whatToDo)
+            {
+                case "+":
+                    {
+                        result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) +
+                                 float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                        break;
+                    }
+
+                case "-":
+                    {
+                        result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) -
+                                 float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                        break;
+                    }
+
+                case "*":
+                    {
+                        result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) *
+                                 float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                        break;
+                    }
+
+                case @"\":
+                    {
+                        result = float.Parse(firstInt, CultureInfo.InvariantCulture.NumberFormat) /
+                                 float.Parse(secondInt, CultureInfo.InvariantCulture.NumberFormat);
+                        break;
+                    }
+            }
+            label1.Text = result.ToString(CultureInfo.InvariantCulture);
+            secondInt = "";
+            firstInt = result.ToString(CultureInfo.InvariantCulture) + " ";
+            whatToDo = "";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (label1.Text.Length != 0)
+                label1.Text = label1.Text.Substring(0, label1.Text.Length - 1);
+            if (!firstInt.EndsWith(" ") && firstInt.Length != 0)
+                firstInt = firstInt.Substring(0, firstInt.Length - 1);
+            else if (secondInt.Length != 0)
+                secondInt = secondInt.Substring(0, secondInt.Length - 1);
+        }
+
+        private void Button15Click(object sender, EventArgs e)
+        {
+            Button Btext = (Button)sender;
+            if (!firstInt.EndsWith(" "))
+                firstInt += Btext.Text;
+            else if (!(label1.Text.Contains("+") || label1.Text.Contains("-") || label1.Text.Contains("*") || label1.Text.Contains(@"\")))
+                return;
+            else
+                secondInt += Btext.Text;
+
+            label1.Text += Btext.Text;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            firstInt = "";
+            secondInt = "";
+            whatToDo = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            string city = "London";
+            object cityObj = comboBox1.SelectedItem;
+            if (cityObj != null)
+                city = cityObj.ToString();
+            string apiKey = keys.weatherKey;
+
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
+            string urlExample =
+                "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=" + apiKey;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            WeatherResponse weatherResponse;
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream ?? throw new InvalidOperationException());
+                string responseFromServer = reader.ReadToEnd();
+
+                weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(responseFromServer);
+            }
+
+            if (weatherResponse != null)
+            {
+                label2.Text = weatherResponse.Name;
+                label3.Text = weatherResponse.Main.Temp.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
+
+namespace WeatherConsoleApplication
+{
+    public class TemperatureInfo
+    {
+        public float Temp { get; set; }
+    }
+}
+
+namespace WeatherConsoleApplication
+{
+    public class WeatherResponse
+    {
+        public TemperatureInfo Main { get; set; }
+
+        public string Name { get; set; }
+    }
+}
